77866485ce56b073535bacee4ad13979d2a4d349
mohamedsafeudeen <mohammed.safeudeen@inceptionlabs.in>
2022-01-03 09:47:40 +0530

    Account details MVC project

---
+++ Controllers/AccountDetails.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class AccountDetails : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> accounts = new List<Account>();
+            accounts.Add(new Account() { AccountID = 123, AccHolderName = "Mohamed Safeudeen", AccHolderAddress = "TamilNadu" });
+            accounts.Add(new Account() { AccountID = 123, AccHolderName = "Mohamed Saif", AccHolderAddress = "TamilNadu" });
+            accounts.Add(new Account() { AccountID = 123, AccHolderName = "Thousif", AccHolderAddress = "TamilNadu" });
+
+            ViewData["Account"] = accounts;
+            return View();
+
+        }   
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication3.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+
+        public string AccHolderName { get; set; }
+
+        public string AccHolderAddress { get; set; }
+
+    }
+}
